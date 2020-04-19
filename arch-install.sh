89d507e768773382cbe5e3941d29664badb60967
grizzlybear1024 <32884451+grizzlybear1024@users.noreply.github.com>
2020-04-18 22:09:08 -0700

    Add files via upload

--- arch-install.sh
+++ arch-install.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 timedatectl set-ntp true
+clear
 
 echo Choose the drive you would like to install arch on:
 fdisk -l | grep 'Disk /'
 read -p 'Drive: ' drive
-clear
+
+read -p 'Would you like to edit the partition table of the drive? (y/n) ' drive_edit_confirm
+
+if [ "$drive_edit_confirm" == "y" ]; then
+        fdisk /dev/sd$drive
+fi
+
 echo Choose the partitions you want arch to install on:
 fdisk -l /dev/sd$drive
 
-read -p 'EFI Partition: ' efi_part
 read -p 'Main Partition: ' main_part
 read -p 'Swap Partition: ' swap_part
 
 mkfs.ext4 /dev/sd$drive$main_part
 mkswap /dev/sd$drive$swap_part
+
+clear
 
 mount /dev/sd$drive$main_part /mnt
 
@@ -22,32 +30,71 @@ pacstrap /mnt base base-devel linux linux-firmware efibootmgr grub nano git dhcp
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt
+read -p 'Would you like to review the file system table? (y/n) ' review_fstab
 
-ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+if [ "$review_fstab" == "y" ]; then
+        nano /mnt/etc/fstab
+	echo 'Is the swap partition missing from the fs table? (y/n) ' add_swap
+	if [ "$add_swap" == "y" ]; then
+		fdisk -l /dev/sd$drive
+		read -p 'Enter the swap partition: ' swap_part
+		echo '' >> /mnt/fstab
+		swap_drive_uuid=$(sudo blkid -s UUID -o value /dev/sd$drive$swap_part)
+		printf "%s" "UUID=" "$swap_drive_uuid"  " none" " swap" " defaults" " 0" " 0" >> /mnt/etc/fstab
+	fi
+fi
+
+clear
+
+echo 'Set system time zone'
+arch-chroot /mnt ls /usr/share/zoneinfo/
+read -p 'Region: ' install_region
+arch-chroot /mnt ls /usr/share/zoneinfo/$install_region
+read -p 'Time Zone: ' time_zone
+
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/$install_region/$time_zone /etc/localtime
 hwclock --systohc
 
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-locale-gen
-echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+arch-chroot /mnt echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+arch-chroot /mnt locale-gen
+arch-chroot /mnt echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
 
 clear
 read -p 'Computer Name: ' pc_name
-echo $pc_name >> /etc/hostname
-echo '127.0.0.1	'$pc_name >> /etc/hosts
-echo '::1		'$pc_name >> /etc/hosts
-echo '127.0.1.1	'$pc_name`.localdomain	'$pc_name
+arch-chroot /mnt echo $pc_name >> /etc/hostname
+arch-chroot /mnt echo '127.0.0.1	'$pc_name >> /etc/hosts
+arch-chroot /mnt echo '::1		'$pc_name >> /etc/hosts
+arch-chroot /mnt echo '127.0.1.1	'$pc_name'.localdomain	'$pc_name >> /etc/hosts
 
 read -p 'Would you like to edit GRUB before making config? (y/n) ' grub_edit_confirm
 
 if [ "$grub_edit_confirm" == "y" ]; then
-	nano /etc/default/grub
+	arch-chroot /mnt nano /etc/default/grub
 fi
 
-mkdir /efi
-mount /dev/sd$drive$efi_part /efi
+echo
 
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt mkdir /efi
+arch-chroot /mnt mount /dev/sd$drive$efi_part /efi
+
+arch-chroot /mnt systemctl enable NetworkManager
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+echo 'Password for root user: '
+arch-chroot /mnt passwd
+
+read -p 'Non-root Username: ' user__name
+arch-chroot /mnt useradd -m -G wheel,audio,video -s /bin/bash $user__name
+echo $user__name'\'s Password:'
+arch-chroot /mnt passwd $user__name
+
+read -p 'Would you like to edit the sudo file? (y/n) ' sudo_edit_confirm
+
+if [ "$sudo_edit_confirm" == "y" ]; then
+        arch-chroot /mnt EDITOR=nano visudo
+fi
+
+arch-chroot /mnt 
 
 echo 'done!'
