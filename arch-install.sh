5a2e22262acf80be2b74c99ed97054aab96fd4ee
grizzlybear1024 <32884451+grizzlybear1024@users.noreply.github.com>
2020-04-19 23:32:56 -0700

    Add files via upload

--- arch-install.sh
+++ arch-install.sh
@@ -1,55 +1,113 @@
 #!/bin/bash
 
 timedatectl set-ntp true
+efi_vars=/sys/firmware/efi/efivars
+total_mem=$(cat /proc/meminfo | grep MemTotal: | cut -d " " -f 8)
+if find "efi_vars" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then #IF SYSTEM IS UEFI
+    efi = 1
+else
+    efi = 0
+fi
 clear
 
-echo '======================\n'
-echo '  DISK SETUP\n'
-echo '======================\n'
+echo '======================'
+echo '  DISK SETUP'
+echo '======================'
+echo ''
 
-echo Choose the drive you would like to install arch on:
-fdisk -l | grep 'Disk /'
-read -p 'Drive: ' drive
+echo 'Choose the drive you would like to install arch on:'
+echo ''
+fdisk -l
+read -p 'Drive (enter the lowercase letter after /dev/sd, for example a for /dev/sda: ' drive
 
-read -p 'Would you like to edit the partition table of the drive? (y/n) ' drive_edit_confirm
+read -p 'Would you like to automatically set up the partitions (WARNING: THIS WILL WIPE ALL DATA FROM THE DRIVE) (y/n)' auto_drive_setup
 
-if [ "$drive_edit_confirm" == "y" ]; then
-        fdisk /dev/sd$drive
+if [ "$auto_drive_setup" == "y" ]; then
+    read -p 'ARE YOU SURE YOU WANT TO WIPE ALL DATA FROM /dev/sd'$drive'? (y/n) ' wipe_confirm
+    
+    if [ "$wipe_confirm" == "y" ]; then
+        if [ "$efi" -eq "1" ]; then #IF SYSTEM IS UEFI
+            sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sd$drive
+    g # clear the in memory partition table
+    n # new partition
+    p # primary partition
+    1 # partition number 1
+    # default - start at beginning of disk 
+    +500M # EFI boot parttion
+    n # new partition
+    p # primary partition
+    2 # partion number 2
+    # default, start immediately after preceding partition
+    -${total_mem}K # default, extend partition to end of disk -RAM
+    n # new partition
+    p # primary partition
+    3 # partion number 1
+    # default, start immediately after preceding partition
+    # end of drive
+    w # write the partition table
+EOF
+            efi_drive = $drive
+            efi_part = 1
+            main_part = 2
+            swap_part = 3
+        else #IF SYSTEM IS NOT UEFI
+            sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sd$drive
+    o # clear the in memory partition table
+    n # new partition
+    p # primary partition
+    1 # partion number 1
+    # default, start immediately after preceding partition
+    -${total_mem}K # default, extend partition to end of disk -RAM
+    n # new partition
+    p # primary partition
+    2 # partion number 1
+    # default, start immediately after preceding partition
+    # end of drive
+    w # write the partition table and save to disk
+EOF
+        main_part = 1
+        swap_part = 2
+        fi
+    fi
+else
+    if [ "$efi" -eq "1" ]; then #IF SYSTEM IS UEFI
+        read -p 'Would you like to manually edit the partition table of the drive? (y/n) ' drive_edit_confirm
+
+        if [ "$drive_edit_confirm" == "y" ]; then
+                clear
+                fdisk /dev/sd$drive
+        fi
+        echo Choose the partitions you want arch to install on:
+        fdisk -l /dev/sd$drive
+
+        read -p 'Main Partition: ' main_part
+        read -p 'Swap Partition: ' swap_part
+
+        read -p 'Is the EFI Partition on another drive? (y/n)' efi_another_drive
+
+        if [ "$efi_another_drive" == "y" ]; then
+            fdisk -l
+            read -p "EFI Drive: " efi_drive
+        else
+            efi_drive=$drive
+        fi
+
+        read -p 'EFI Partition: ' efi_part
+    else #IF SYSTEM IS NOT UEFI
+        efi = 0
+        read -p 'Would you like to manually edit the partition table of the drive? (y/n) ' drive_edit_confirm
+
+        if [ "$drive_edit_confirm" == "y" ]; then
+                clear
+                fdisk /dev/sd$drive
+        fi
+    fi
 fi
-
-echo Choose the partitions you want arch to install on:
-fdisk -l /dev/sd$drive
-
-read -p 'Main Partition: ' main_part
-read -p 'Swap Partition: ' swap_part
-
-mkfs.ext4 /dev/sd$drive$main_part
-mkswap /dev/sd$drive$swap_part
-
 clear
 
-echo
-
-echo '======================\n'
-echo '  INTERNET SETUP\n'
-echo '======================\n'
-
-read -p 'WiFi or Ethernet? (w/e)' wifi_eth
-
-if [ "$wifi_eth" == "w" ]; then
-	wifi-menu
-fi
-
-read -p "Test ping archlinux.org? (y/n) " test_ping
-if [ "$test_ping" == "y" ]; then
-        ping archlinux.org -c 4
-fi
-
-clear
-
-echo '======================\n'
-echo '  PACKAGE INSTALL\n'
-echo '======================\n'
+echo '======================'
+echo '  PACKAGE INSTALL'
+echo '======================'
 
 
 mount /dev/sd$drive$main_part /mnt
@@ -62,21 +120,22 @@ read -p 'Would you like to review the file system table? (y/n) ' review_fstab
 
 if [ "$review_fstab" == "y" ]; then
         nano /mnt/etc/fstab
-	echo 'Is the swap partition missing from the fs table? (y/n) ' add_swap
+        clear
+	read -p 'Is the swap partition missing from the fs table? (y/n) ' add_swap
 	if [ "$add_swap" == "y" ]; then
 		fdisk -l /dev/sd$drive
 		read -p 'Enter the swap partition: ' swap_part
 		echo '' >> /mnt/fstab
-		swap_drive_uuid=$(sudo blkid -s UUID -o value /dev/sd$drive$swap_part)
+		swap_drive_uuid=$(blkid -s UUID -o value /dev/sd$drive$swap_part)
 		printf "%s" "UUID=" "$swap_drive_uuid"  " none" " swap" " defaults" " 0" " 0" >> /mnt/etc/fstab
 	fi
 fi
 
 clear
 
-echo '======================\n'
-echo '  TIME / LOCALES\n'
-echo '======================\n'
+echo '======================'
+echo '  TIME / LOCALES'
+echo '======================'
 
 
 echo 'Set system time zone'
@@ -86,82 +145,67 @@ arch-chroot /mnt ls /usr/share/zoneinfo/$install_region
 read -p 'Time Zone: ' time_zone
 
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/$install_region/$time_zone /etc/localtime
-hwclock --systohc
+arch-chroot /mnt hwclock --systohc
 
-arch-chroot /mnt echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'en_US.UTF-8 UTF-8' >> /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
-arch-chroot /mnt echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+echo 'LANG=en_US.UTF-8' >> /mnt/etc/locale.conf
 
 clear
 
-echo '======================\n'
-echo '  NETWORK SETUP\n'
-echo '======================\n'
+echo '======================'
+echo '  NETWORK SETUP'
+echo '======================'
 
 echo 'Starting and enabling Network Manager...'
 
 arch-chroot /mnt systemctl start NetworkManager
 arch-chroot /mnt systemctl enable NetworkManager
 
-if [ "$wifi_eth" == "w" ]; then
-	arch-chroot /mnt nmcli r wifi on
-	echo '\nLog into wifi with NetworkManager'
-	echo 'Choose your network'
-	arch-chroot /mnt nmcli d wifi list
-	read -p 'SSID: ' ssid
-	read -p -s 'Wifi Password: ' wifi_pass
-	arch-chroot /mnt nmcli d wifi connect $ssid password $wifi_pass
-	read -p "Test ping archlinux.org? (y/n) " test_ping
-	if [ "$test_ping" == "y" ]; then
-        	ping archlinux.org -c 4
-	fi
-fi
-
 read -p 'Computer Name: ' pc_name
-arch-chroot /mnt echo $pc_name >> /etc/hostname
-arch-chroot /mnt echo '127.0.0.1	'$pc_name >> /etc/hosts
-arch-chroot /mnt echo '::1		'$pc_name >> /etc/hosts
-arch-chroot /mnt echo '127.0.1.1	'$pc_name'.localdomain	'$pc_name >> /etc/hosts
+echo $pc_name >> /mnt/etc/hostname
+echo '127.0.0.1	'$pc_name >> /mnt/etc/hosts
+echo '::1		'$pc_name >> /mnt/etc/hosts
+echo '127.0.1.1	'$pc_name'.localdomain	'$pc_name >> /mnt/etc/hosts
 
 clear
 
-echo '======================\n'
-echo '  GRUB SETUP\n'
-echo '======================\n'
+echo '======================'
+echo '  GRUB SETUP'
+echo '======================'
 
 
 read -p 'Would you like to edit GRUB before making config? (y/n) ' grub_edit_confirm
 
 if [ "$grub_edit_confirm" == "y" ]; then
 	arch-chroot /mnt nano /etc/default/grub
+	clear
+fi
+if [ "$efi" -eq "1" ]; then
+    arch-chroot /mnt mkdir /efi
+    arch-chroot /mnt mount /dev/sd$efi_drive$efi_part /efi
+
+    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+    arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+else
+    grub-install --target=i386-pc /dev/sd$drive
+    arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
-
-arch-chroot /mnt mkdir /efi
-arch-chroot /mnt mount /dev/sd$drive$efi_part /efi
-
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-
-echo '======================\n'
-echo '  USER / PERMS SETUP\n'
+echo '======================'
+echo '  USER / PERMS SETUP'
 echo '======================'
 
 
 echo 'Password for root user: '
 arch-chroot /mnt passwd
 
-read -p 'Non-root Username: ' user__name
+read -p 'sudo Username: ' user__name
 arch-chroot /mnt useradd -m -G wheel,audio,video -s /bin/bash $user__name
-echo $user__name'\'s Password:'
+echo 'Password:'
 arch-chroot /mnt passwd $user__name
 
-read -p 'Would you like to edit the sudo file? (y/n) ' sudo_edit_confirm
-
-if [ "$sudo_edit_confirm" == "y" ]; then
-        arch-chroot /mnt EDITOR=nano visudo
-fi
-
+echo '%wheel ALL=(ALL) ALL' >> /mnt/etc/sudoers
 clear
 
 read -p 'Installation and setup has completed. Would you like to reboot or chroot into your system: (r/c) ' reboot_or_chroot
